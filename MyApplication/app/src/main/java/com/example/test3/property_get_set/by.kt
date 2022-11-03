b7d416dbee1079b33b3a776b52b4f91041aaa968
kimbs95 <lifee456@naver.com>
2022-11-03 17:26:54 +0900

    2022.11.03

--- MyApplication/app/src/main/java/com/example/test3/property_get_set/by.kt
+++ MyApplication/app/src/main/java/com/example/test3/property_get_set/by.kt
@@ -15,6 +15,9 @@ class SportImpl(val power :String ) :Car{
     override fun go() = "는 경주용에 사용되며 $power 마력을 가집니다."
 }
 class CarModel (val model :String, impl :Car) : Car by impl{ // Car에 정의 되어있는것을 impl 이 다 잡아 먹고 impl을 사용할수 있도록 해준다
+    //    interface open 을 기본적으로 가져서
+    //    CarModel 이  interface Car 를 받으면 Car 에 정의된 걸 override 로 받아야하지만
+    //    impl 에 의해 override 없이 사용 가능하다
     fun carInfo(){
         println("$model ${go()}") //참조 없이 각 인터페이스 구현클래스의 go를 접근
     }
