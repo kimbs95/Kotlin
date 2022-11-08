b0e4e5d96bd5d61c1d148f73331e6aae11db77b9
kimbs95 <lifee456@naver.com>
2022-11-08 16:22:27 +0900

    2022.11.08

--- MyApplication7/app/src/main/java/com/example/myapplication7/MainActivity.kt
+++ MyApplication7/app/src/main/java/com/example/myapplication7/MainActivity.kt
@@ -7,15 +7,18 @@ import com.example.myapplication7.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private var mBinding:ActivityMainBinding? = null
     private val binding get() =mBinding!!
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {// 뷰에 접근할수있음
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        mBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)// inflate(layoutInflater) 같이 써준다
+        //바인딩 클래스 이름은 규칙이 정해져있다
+        //Binding class 이름 ActivityMainBinding = MainActivity Activity 이름
+        setContentView(binding.root)// .root 로 진입
         binding.tvMessage.setText("안녕하세요 김병성입니다.")
     }
     override fun onDestroy(){
         mBinding = null
+        // 컴파일 시 오류 가 생길수있어 null 로 변경
         super.onDestroy()
     }
 }
